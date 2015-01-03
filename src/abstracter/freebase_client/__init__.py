d2bdc00f033aba46bb1bc8807967b1a58f945f9b
Schrottenloher <andre.schrottenloher@polytechnique.edu>
2015-01-03 17:13:00 +0100

    Created the first real network.

--- src/abstracter/freebase_client/__init__.py
+++ src/abstracter/freebase_client/__init__.py
@@ -2,7 +2,10 @@ import json
 import codecs
 import urllib.request
 import urllib.parse
-from settings import *
+try:
+	from abstracter.freebase_client.settings import *
+except ImportError:
+	from settings import *
 
 reader = codecs.getreader("utf-8")
 
@@ -20,16 +23,31 @@ def search(lang='en',limit=10,**kwargs):
 		else:
 			pass
 	url_values = urllib.parse.urlencode(data)
-	print("searching freebase : "+url_values)
+	#print("searching freebase : "+url_values)
 	full_url = URL + '?' + url_values
 	resp = urllib.request.urlopen(full_url)
 	return json.load(reader(resp))['result']
 
+def search_name(name):
+	data=search(query=name,lang='en',limit=2)
+	res={}
+	if data:
+		dat=data[0]
+		score=dat['score']
+		name=dat['name']
+		res['from']=name.lower().replace(' ','_')
+		res['score']=score
+		if 'notable' in dat:
+			res['to']=dat['notable']['name'].lower().replace(' ','_')#IsA relation
+	return res
 
 
 if __name__=="__main__":
 
-	#print (search(query='barack obama',lang='en',filter='(any type:/people/person)'))
+	#for i in (search(query='ezequiel lavezzi',lang='en',filter='(any type:/people/person)',limit=2)):
+	#	print(i)
+	#print((search(query='ezequiel lavezzi',lang='en',filter='(any type:/people/person)',limit=2))[0])
 	#print (search(query='barack_obama',lang='en',filter='(any type:/people/person)',limit=2,exact=True))
-	print (search(filter='(all type:/people/person member_of:france)',limit=10,exact=False))
+	#print (search(filter='(all type:/people/person member_of:france)',limit=10,exact=False))
 #	print(search(filter='(all discovered_by:heisenberg)',limit=10))
+	print(search_name("albert rusnak"))
--- src/parse_crawler.py
+++ src/parse_crawler.py
@@ -18,24 +18,25 @@ def parse_article(filename):
 	return []
 
 
-DEFAULT_DATA_DIRECTORY="crawlerpsc/2014_12_04/"
-DEFAULT_RESULTS_DIRECTORY="concepts/2014_12_04/"
-DEFAULT_CONCEPTS_FILE="all_concepts.jsons"
-DEFAULT_NAMES_FILE="all_names.jsons"
+DEFAULT_DATA_DIRECTORY="crawlerpsc/"
+DEFAULT_RESULTS_DIRECTORY="concepts/"
+CONCEPTS_NAMES_DATA_DIRECTORY="concepts_names_data/"
+#DEFAULT_CONCEPTS_FILE="all_concepts.jsons"
+#DEFAULT_NAMES_FILE="all_names.jsons"
 
 def parse_directory(max_subdirectories=10,max_files=100,data_directory=DEFAULT_DATA_DIRECTORY,
-	results_directory=DEFAULT_RESULTS_DIRECTORY):
-	if not os.path.isdir(results_directory):
-		os.makedirs(results_directory)
+	results_directory=DEFAULT_RESULTS_DIRECTORY,subdirectory="2014_12_04"):
+	if not os.path.isdir(results_directory+subdirectory+"/"):
+		os.makedirs(results_directory+subdirectory+"/")
 	i=0
 	j=0
-	while(os.path.exists(data_directory+"%i/%i" % (i,j)) and i<max_subdirectories):
-		while(os.path.exists(data_directory+"%i/%i" % (i,j)) and j<max_files):
-			temp=parse_article(data_directory+"%i/%i" % (i,j))
+	while(os.path.exists(data_directory+"%s/%i/%i" % (subdirectory,i,j)) and i<max_subdirectories):
+		while(os.path.exists(data_directory+"%s/%i/%i" % (subdirectory,i,j)) and j<max_files):
+			temp=parse_article(data_directory+"%s/%i/%i" % (subdirectory,i,j))
 			if temp:
-				with open(results_directory+"%i_%i_concepts.json" % (i,j),'w') as file:
+				with open(results_directory+"%s/%i_%i_concepts.json" % (subdirectory,i,j),'w') as file:
 					json.dump(temp[0],file)
-				with open(results_directory+"%i_%i_names.json" % (i,j),'w') as file:
+				with open(results_directory+"%s/%i_%i_names.json" % (subdirectory,i,j),'w') as file:
 					json.dump(temp[1],file)
 					print("successful with : "+data_directory+"%i/%i" % (i,j))
 			j+=1
@@ -45,8 +46,8 @@ def parse_directory(max_subdirectories=10,max_files=100,data_directory=DEFAULT_D
 
 
 
-def unify_all_names(directory=DEFAULT_RESULTS_DIRECTORY,max_files=10,
-	names_file=DEFAULT_NAMES_FILE):
+def unify_all_names(directory=DEFAULT_RESULTS_DIRECTORY,max_files=10,subdirectory="2014_12_04",
+	names_file=None):
 	"""
 	Retrieve all names and put them into a single dict.
 	The value associated to each name represents the number of articles in which it appears.
@@ -57,11 +58,11 @@ def unify_all_names(directory=DEFAULT_RESULTS_DIRECTORY,max_files=10,
 	j=0
 	all_names={}
 	k=0
-	while(os.path.exists(directory+"%i_%i_names.json" % (i,j)) and k<max_files):
-		while(os.path.exists(directory+"%i_%i_names.json" % (i,j)) and k<max_files):
+	while(os.path.exists(directory+"%s/%i_%i_names.json" % (subdirectory,i,j)) and k<max_files):
+		while(os.path.exists(directory+"%s/%i_%i_names.json" % (subdirectory,i,j)) and k<max_files):
 			k+=1
 			temp=[]
-			with open(directory+"%i_%i_names.json" % (i,j),'r') as file:
+			with open(directory+"%s/%i_%i_names.json" % (subdirectory,i,j),'r') as file:
 				temp=json.load(file)
 				print("successful loading of "+directory+"%i/%i" % (i,j))
 			if temp:
@@ -73,14 +74,18 @@ def unify_all_names(directory=DEFAULT_RESULTS_DIRECTORY,max_files=10,
 			j+=1
 		j=0
 		i+=1	
+	if not names_file:
+		if not os.path.isdir(CONCEPTS_NAMES_DATA_DIRECTORY):
+			os.makedirs(CONCEPTS_NAMES_DATA_DIRECTORY)
+		names_file=CONCEPTS_NAMES_DATA_DIRECTORY+subdirectory+"_all_names.jsons"
 	writer=JSONStreamWriter(names_file)
 	for d in all_names.items():
 		writer.write(d)
 	writer.close()
 
 
-def unify_all_concepts(directory=DEFAULT_RESULTS_DIRECTORY,max_files=10,
-	concepts_file=DEFAULT_CONCEPTS_FILE):
+def unify_all_concepts(directory=DEFAULT_RESULTS_DIRECTORY,max_files=10,subdirectory="2014_12_04",
+	concepts_file=None):
 	"""
 	Retrieve all concepts and put them into a single dict.
 	The result is put into a single jsonstream.
@@ -90,11 +95,11 @@ def unify_all_concepts(directory=DEFAULT_RESULTS_DIRECTORY,max_files=10,
 	j=0
 	all_concepts={}
 	k=0
-	while(os.path.exists(directory+"%i_%i_concepts.json" % (i,j)) and k<max_files):
-		while(os.path.exists(directory+"%i_%i_concepts.json" % (i,j)) and k<max_files):
+	while(os.path.exists(directory+"%s/%i_%i_concepts.json" % (subdirectory,i,j)) and k<max_files):
+		while(os.path.exists(directory+"%s/%i_%i_concepts.json" % (subdirectory,i,j)) and k<max_files):
 			k+=1
 			temp=[]
-			with open(directory+"%i_%i_concepts.json" % (i,j),'r') as file:
+			with open(directory+"%s/%i_%i_concepts.json" % (subdirectory,i,j),'r') as file:
 				temp=json.load(file)
 				print("successful loading of "+directory+"%i/%i" % (i,j))
 			if temp:
@@ -106,6 +111,10 @@ def unify_all_concepts(directory=DEFAULT_RESULTS_DIRECTORY,max_files=10,
 			j+=1
 		j=0
 		i+=1	
+	if not concepts_file:
+		if not os.path.isdir(CONCEPTS_NAMES_DATA_DIRECTORY):
+			os.makedirs(CONCEPTS_NAMES_DATA_DIRECTORY)
+		concepts_file=CONCEPTS_NAMES_DATA_DIRECTORY+subdirectory+"_all_concepts.jsons"	
 	writer=JSONStreamWriter(concepts_file)
 	for d in all_concepts.items():
 		writer.write(d)
@@ -142,13 +151,64 @@ def fusion(max_files=1000):
 		writer.write(d)
 	writer.close()
 
+from re import match
+from glob import glob
+
+def unify(directory=CONCEPTS_NAMES_DATA_DIRECTORY,max_files=10,names_file="names.jsons",concepts_file="concepts.jsons"):
+	"""
+	Unifies all dicts of concepts and list of names into two single files.
+	Retrieves all data from the default directory, with a maximum number of files.
+	Suppress bad characters.
+	"""
+	#(_, _, filenames) = os.walk(directory).next()
+	filenames=glob(directory+"*names.jsons")[0:max_files]
+	fileconcepts=glob(directory+"*concepts.jsons")[0:max_files]
+	thedict={}
+	for name in filenames:
+		for c in read_json_stream(name):
+			#avoid bad words
+			if match('^[a-zA-Z\s-]*$',c[0]) and len(c[0]) < 20:
+				if c[0] not in thedict:
+					thedict[c[0]]=1
+				else:
+					thedict[c[0]]+=c[1]
+		print("successful reading of :"+name)
+	writer=JSONStreamWriter(names_file)
+	for d in thedict.items():
+		writer.write(d)
+	writer.close()
+	thedict={}
+	for name in fileconcepts:
+		for c in read_json_stream(name):
+			#avoid bad words
+			if match('^[a-zA-Z\s-]*$',c[0]) and len(c[0]) < 20:
+				if c[0] not in thedict:
+					thedict[c[0]]=1
+				else:
+					thedict[c[0]]+=c[1]
+		print("successful reading of :"+name)
+	writer=JSONStreamWriter(concepts_file)
+	for d in thedict.items():
+		writer.write(d)
+	writer.close()
+
+
+
+
+
 ###################################################
 ####EXAMPLE
 ###################################################
 
-#parse_directory(max_subdirectories=10,max_files=100,data_directory="crawlerpsc/2014_12_05/",results_directory="concepts/2014_12_05/")
-#unify_all_names(directory="concepts/2014_12_05/",max_files=1000,names_file="all_names_2014_12_05.jsons")
-#unify_all_concepts(directory="concepts/2014_12_05/",max_files=1000,concepts_file="all_concepts_2014_12_05.jsons")
+#parse_directory(max_subdirectories=10,max_files=100,data_directory="crawlerpsc/2015_01_01/",results_directory="concepts/2015_01_01/")
+#unify_all_names(subdirectory="2015_01_01",max_files=1000,names_file="all_names_2015_01_01.jsons")
+#unify_all_concepts(directory="concepts/2014_12_04/",max_files=1000,concepts_file="all_concepts_2014_12_04.jsons")
 
+#parse_directory(max_subdirectories=10,max_files=100,data_directory="crawlerpsc/",
+#	results_directory="concepts/",subdirectory="2015_01_02")
 
+#parse_directory(max_subdirectories=10,max_files=100,subdirectory="2015_01_02")
+#unify_all_names(subdirectory="2015_01_02",max_files=1000)
+#unify_all_concepts(subdirectory="2015_01_02",max_files=1000)
 
+#unify()
